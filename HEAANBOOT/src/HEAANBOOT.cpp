27a2db67e64e08c523fba5d51b7e683a7115c038
kimandrik <kimandrik@192.168.0.34>
2017-10-10 12:21:51 +0900

    kimandrik:init commit

---
+++ HEAANBOOT/src/HEAANBOOT.cpp
@@ -0,0 +1,20 @@
+#include "TestBoot.h"
+#include <TestScheme.h>
+
+int main() {
+
+//	TestMatrix::testMatrix(13, 130, 30);
+//	TestHD::testHD(13, 75, 30, 11);
+//	TestHD::testDNA(13, 75, 30);
+//	TestCompare::testOrder();
+//	TestCompare::testSort();
+
+	TestBoot::testBootstrap();
+//	TestBoot::testBootstrapOneReal();
+//	TestBoot::testBoundOfI();
+
+//	TestBoot::test();
+
+	return 0;
+}
+
